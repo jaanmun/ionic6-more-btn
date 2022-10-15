c96f87f22f3b0fcf0c6b26bc2914fa09aa6d09d2
JaanMun <yjm7575@naver.com>
2022-10-15 22:05:52 +0900

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
+const MAX_COUNT = 7;
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild('text') text: ElementRef;
 
-  constructor() {}
+  count = 2; // default
+  isOpen = false;
+
+  constructor(private renderer: Renderer2) {}
+
+  inc(n: number) {
+    this.count = Math.min(Math.max(1, this.count + n), MAX_COUNT);
+    this.renderer.setStyle(this.text.nativeElement, 'webkitLineClamp', this.count);
+  }
+
+  more() {
+    if (this.isOpen) {
+      this.renderer.setStyle(this.text.nativeElement, 'display', '-webkit-box');
+      this.isOpen = false; 
+    } else {
+      this.renderer.setStyle(this.text.nativeElement, 'display', 'block');
+      this.isOpen = true; 
+    }
+  }
 
 }
